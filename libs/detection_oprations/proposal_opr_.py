e893439c458ddf2c775d0d148cae4b2d69dfd081
yangxue <yangxue0827@126.com>
2020-08-29 18:59:06 +0800

    significantly improve test performance

--- libs/detection_oprations/proposal_opr_.py
+++ libs/detection_oprations/proposal_opr_.py
@@ -54,7 +54,7 @@ def postprocess_detctions(rpn_bbox_pred, rpn_cls_prob, anchors, is_training):
         nms_indices = nms_rotate.nms_rotate(decode_boxes=boxes_pred,
                                             scores=scores,
                                             iou_threshold=cfgs.NMS_IOU_THRESHOLD,
-                                            max_output_size=100,
+                                            max_output_size=100 if is_training else 1000,
                                             use_angle_condition=False,
                                             angle_threshold=15,
                                             use_gpu=False)
